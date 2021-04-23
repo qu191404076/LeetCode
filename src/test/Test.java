8f3e26675a5a82e7cc3bb8706b7b9da70ac0b068
qu <123@qq.com>
2021-04-23 20:28:09 +0800

    update Test

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,5 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("IDEA create branch test!");
+        System.out.println("error");
     }
 }
