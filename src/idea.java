2310b703af81bcee72b0b58bfe75122e300e11ef
qu <123@qq.com>
2021-08-30 17:47:48 +0800

    create idea.java print idea test branch

---
+++ src/idea.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName idea
+ * @Description print idea test branch
+ * @Author QuZheng
+ * @Date 2021/8/30 17:43:56
+ * @Version 1.0
+ */
+public class idea {
+    public static void main(String[] args) {
+        System.out.println("idea test branch");
+    }
+}
